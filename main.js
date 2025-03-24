653127950b09fdf4615231d3f11ff36719d44f78
JC <jcbrady89@yahoo.com>
2025-03-24 10:56:38 -0600

    making dyamic weather call work

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/SLC/101,175/forecast")
-  //   const weatherData = await weatherPromise
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+
   // Brad says this is plain text that we're parsing with json() method
   const weatherData = await weatherPromise.json()
   const ourTemperature = weatherData.properties.periods[0].temperature
 
-  console.log(ourTemperature)
+  document.querySelector("#tempOutput").textContent = ourTemperature
 
-  console.log(weatherData.properties.periods[0])
-  console.log(weatherData.properties.periods[0].detailedForecast)
-  console.log(weatherData.properties.periods[0].shortForecast)
-  console.log(weatherData.properties.periods[0].windDirection)
-  console.log(weatherData.properties.periods[0].windSpeed)
-  console.log(weatherData.properties.periods[0].temperature)
+  //   console.log(weatherData.properties.periods[0])
+  //   console.log(weatherData.properties.periods[0].detailedForecast)
+  //   console.log(weatherData.properties.periods[0].shortForecast)
+  //   console.log(weatherData.properties.periods[0].windDirection)
+  //   console.log(weatherData.properties.periods[0].windSpeed)
+  //   console.log(weatherData.properties.periods[0].temperature)
 }
 
 start()
