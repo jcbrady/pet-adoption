50d029a50f78e89d77176618a95eb0e920dae3a8
JC <jcbrady89@yahoo.com>
2025-03-25 15:17:03 -0600

    cleaned up unnecessary comments and console logs

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const template = document.querySelector("#pet-card-template")
 const wrapper = document.createDocumentFragment()
-// console.log(template)
-// console.log(template.content.cloneNode(true))
-// console.log(wrapper)
 
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
@@ -12,12 +9,6 @@ async function start() {
   const ourTemperature = weatherData.properties.periods[0].temperature
 
   document.querySelector("#tempOutput").textContent = ourTemperature
-
-  //   console.log(weatherData.properties.periods[0])
-  //   console.log(weatherData.properties.periods[0].detailedForecast)
-  //   console.log(weatherData.properties.periods[0].shortForecast)
-  //   console.log(weatherData.properties.periods[0].windDirection)
-  //   console.log(weatherData.properties.periods[0].windSpeed)
 }
 
 start()
@@ -25,9 +16,6 @@ start()
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   petsData = await petsPromise.json()
-
-  //   console.log(petsPromise)
-  //   console.log(petsData)
 
   petsData.forEach((pet, index) => {
     const clone = template.content.cloneNode(true)
@@ -40,28 +28,7 @@ async function petsArea() {
     clone.querySelector(".pet-card-photo img").src = pet.photo
     clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
 
-    // if (!pet.photo) {
-    //   clone.querySelector(".pet-card-photo img").src = "/images/fallback.jpg"
-    //   clone.querySelector(".pet-card-photo img").alt = `no photo yet for a ${pet.species} named ${pet.name}`
-    // } else {
-    //   clone.querySelector(".pet-card-photo img").src = pet.photo
-    //   clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
-    // }
-
     wrapper.appendChild(clone)
-
-    // const petsArea = document.querySelector(".list-of-pets")
-    // const card = `<div class="pet-card">
-    //         <div class="pet-card-text">
-    //           <h3>${pet.name}</h3>
-    //           <p class="pet-description">${pet.description}</p>
-    //           <p class="pet-age">${pet.birthYear}</p>
-    //         </div>
-    //         <div class="pet-card-photo">
-    //           <img src="images/barksalot.jpg" alt="${pet.description}" />
-    //         </div>
-    //       </div>`
-    // petsArea.innerHTML += card
   })
 
   const petsArea = document.querySelector(".list-of-pets")
@@ -83,21 +50,7 @@ function createAgeText(birthYear) {
 
 // pet filter button code
 const buttons = document.querySelectorAll(".pet-filter button")
-buttons.forEach(btn => {
-  btn.addEventListener("click", handleButtonClick)
-  // e => {
-  // console.log(e.target.classList.value)
-
-  // if (e.target.classList.value == "") {
-  //   e.target.classList.add("active")
-  //   buttons.forEach(b => {
-  //     b.classList.remove("active")
-  //   })
-  // } else {
-  //   e.target.classList.remove("active")
-  // }
-  //   })
-})
+buttons.forEach(btn => btn.addEventListener("click", handleButtonClick))
 
 function handleButtonClick(e) {
   // remove active class from any and all buttons
