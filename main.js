ad438a5bd3067607ded48d8daacf16f33badac3d
JC <jcbrady89@yahoo.com>
2025-03-24 18:12:23 -0600

    turned pet card into a JS  template

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+console.log(template)
+console.log(template.content.cloneNode(true))
+// console.log(wrapper)
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
 
@@ -24,20 +30,29 @@ async function petsArea() {
   //   console.log(petsPromise)
   console.log(petsData)
 
-  petsData.forEach(pet => {
-    const card = `<div class="pet-card">
-            <div class="pet-card-text">
-              <h3>${pet.name}</h3>
-              <p class="pet-description">${pet.description}</p>
-              <p class="pet-age">${pet.birthyear}</p>
-            </div>
-            <div class="pet-card-photo">
-              <img src="images/barksalot.jpg" alt="A white fluffy dog named Barskalot" />
-            </div>
-          </div>`
-    console.log(card)
-    document.body.append(card)
+  petsData.forEach((pet, index) => {
+    const clone = template.content.cloneNode(true)
+
+    clone.querySelector("h3").textContent = pet.name
+
+    wrapper.appendChild(clone)
+
+    // const petsArea = document.querySelector(".list-of-pets")
+    // const card = `<div class="pet-card">
+    //         <div class="pet-card-text">
+    //           <h3>${pet.name}</h3>
+    //           <p class="pet-description">${pet.description}</p>
+    //           <p class="pet-age">${pet.birthYear}</p>
+    //         </div>
+    //         <div class="pet-card-photo">
+    //           <img src="images/barksalot.jpg" alt="${pet.description}" />
+    //         </div>
+    //       </div>`
+    // petsArea.innerHTML += card
   })
+
+  const petsArea = document.querySelector(".list-of-pets")
+  petsArea.appendChild(wrapper)
 }
 
 petsArea()
